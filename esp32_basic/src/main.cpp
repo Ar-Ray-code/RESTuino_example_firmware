71769c8f34f232cce95ed1388099ed105ee32a69
Ar-Ray-code <ray255ar@gmail.com>
2022-09-20 19:48:41 +0900

    add source code

---
+++ esp32_basic/src/main.cpp
@@ -0,0 +1,16 @@
+#include "RESTuino/restuino_func.h"
+
+uint8_t len_ssid = 1;
+const char *ssid_def[] = {"SVDLab-link"};
+const char *ssid_pass[] = {"0zuno224"};
+
+static RestuinoFunc func;
+
+void setup()
+{
+  func.restuino_setup();
+}
+void loop()
+{
+  func.restuino_loop();
+}
